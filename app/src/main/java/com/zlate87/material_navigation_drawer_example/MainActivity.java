671176945a7fcf4f626ebeb5d0d5414df93f5526
Zlatko <Zlatko.Stamatov@gmail.com>
2014-11-23 13:51:02 +0100

    Removed unused imports.

--- app/src/main/java/com/zlate87/material_navigation_drawer_example/MainActivity.java
+++ app/src/main/java/com/zlate87/material_navigation_drawer_example/MainActivity.java
@@ -1,10 +1,7 @@
 package com.zlate87.material_navigation_drawer_example;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.SearchManager;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
@@ -13,17 +10,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
